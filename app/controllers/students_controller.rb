07842ea4e49b807b23491a82c2f66e6af4d2b4c3
Drew Thompson <drewallynthompson@gmail.com>
2020-11-26 07:36:13 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,14 @@
 class StudentsController < ApplicationController
   
+  
+  
   private
   
-  def finder
-    @student = Student.find(params[:id])
-  end 
+    def finder
+      @student = Student.find(params[:id])
+    end 
   
-  def student_params
-    
-  end
+    def student_params
+      params.require(:student).permit(:first_name, :last_name)
+    end
 end 
