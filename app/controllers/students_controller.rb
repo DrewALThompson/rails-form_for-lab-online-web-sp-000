0a0447407587fd6d425859f0ab9d332911c5359a
Drew Thompson <drewallynthompson@gmail.com>
2020-11-26 07:38:00 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,28 @@
 class StudentsController < ApplicationController
   
+  def index
+    @students = Student.all
+  end 
+  
+  def show
+    student_finder
+  end 
+  
+  def new
+  end 
+  
+  def create
+    @student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
+  end 
+  
+  def 
   
   
   private
   
-    def finder
+    def student_finder
       @student = Student.find(params[:id])
     end 
   
